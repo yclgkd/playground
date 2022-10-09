0e10d0b3fe663c5fd168a04f5e114af12ae41091
brian <chunlai0928@gmail.com>
2022-10-10 01:14:44 +0800

    feat: react router tutorial

---
+++ apps/react-router-tutorial/src/app/App.js
@@ -0,0 +1,10 @@
+import NxWelcome from './nx-welcome';
+export function App() {
+  return (
+    <>
+      <NxWelcome title="react-router-tutorial" />
+      <div />
+    </>
+  );
+}
+export default App;
